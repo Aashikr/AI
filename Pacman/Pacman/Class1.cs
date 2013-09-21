7c7fb12f11547ecf0a5e543b8fb9c505c84bd93b
Aditya Ahuja <aditya.ahuja123@gmail.com>
2013-09-21 15:26:46 -0500

    Initializing repo
    Committing the latest version of scripts as of now

---
+++ Pacman/Pacman/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pacman
+{
+    class G
+    {
+        public static int z;
+        public static int i,j;
+        public static int[] stack;
+        public static int top;
+            
+    }
+}
